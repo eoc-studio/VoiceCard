3153777f723e561a321f8523c06a328f92c9b64b
Kane-Shih <keyxun@hotmail.com>
2014-01-25 19:11:50 +0800

    Add FlipView

---
+++ src/eoc/studio/voicecard/animation/FlipView.java
@@ -0,0 +1,190 @@
+package eoc.studio.voicecard.animation;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class FlipView extends RelativeLayout
+{
+	private static final String TAG = "FlipView";
+	private static final long DEFAULT_FLIP_DURATION_MS = 400;
+	private static final float DEFAULT_ROTATE_BEGIN = -45f;
+	private static final float DEFAULT_ROTATE_END = 0;
+	private final Interpolator ACCELERATOR = new AccelerateInterpolator();
+	private final Interpolator DECELERATOR = new DecelerateInterpolator();
+	private boolean isFlipping = false;
+	private FrameLayout frontPage;
+	private FrameLayout backPage;
+	private FrameLayout innerPage;
+	private int pageWidth;
+	private int pageHeight;
+	private long flipDuration = DEFAULT_FLIP_DURATION_MS;
+	private float pivotY;
+	private float rotationBegin = DEFAULT_ROTATE_BEGIN;
+	private float rotationEnd = DEFAULT_ROTATE_END;
+	private final float[] ROTATE_FRONT_DEGREE1 = { rotationBegin, -90f };
+	private final float[] ROTATE_FRONT_DEGREE2 = { 90f, rotationEnd };
+	private final float[] ROTATE_BACK_DEGREE1 = { rotationEnd, 90f };
+	private final float[] ROTATE_BACK_DEGREE2 = { -90f, rotationBegin };
+	private final OnTouchListener flipListener = new OnTouchListener()
+	{
+		@Override
+		public boolean onTouch(View v, MotionEvent event)
+		{
+			flip(frontPage, backPage);
+			return false;
+		}
+	};
+	
+	public FlipView(Context context, int width, int height, float rotationBegin, float rotationEnd, float pivotY)
+	{
+		super(context);
+		this.rotationBegin = rotationBegin;
+		this.rotationEnd = rotationEnd;
+		this.pivotY = pivotY;
+		
+		LayoutParams params = new LayoutParams(width, height);
+		setLayoutParams(params);
+		frontPage = new FrameLayout(context);
+		backPage = new FrameLayout(context);
+		innerPage = new FrameLayout(context);
+		pageWidth = width / 2;
+		pageHeight = height * 2 / 3;
+		Log.d(TAG, "page w,h " + pageWidth + ", " + pageHeight);
+		LayoutParams oddParams = new LayoutParams(pageWidth, pageHeight);
+		oddParams.addRule(CENTER_VERTICAL);
+		oddParams.addRule(ALIGN_PARENT_RIGHT);
+		oddParams.leftMargin = pageWidth;
+		frontPage.setLayoutParams(oddParams);
+		innerPage.setLayoutParams(oddParams);
+		LayoutParams evenParams = new LayoutParams(pageWidth, pageHeight);
+		evenParams.addRule(CENTER_VERTICAL);
+		evenParams.addRule(ALIGN_PARENT_LEFT);
+		backPage.setLayoutParams(evenParams);
+		backPage.setVisibility(INVISIBLE);
+
+		frontPage.setOnTouchListener(flipListener);
+		backPage.setOnTouchListener(flipListener);
+		
+		frontPage.setPivotY(pivotY);
+		frontPage.setRotationY(rotationBegin);
+		backPage.setPivotY(pivotY);
+		backPage.setRotationY(rotationBegin);
+		
+		addView(innerPage);
+		addView(backPage);
+		addView(frontPage);
+	}
+
+	public void setFrontPage(View v)
+	{
+		frontPage.removeAllViews();
+		frontPage.addView(v);
+	}
+
+	public View getFrontPage()
+	{
+		return frontPage.getChildAt(0);
+	}
+
+	public void setBackPage(View v)
+	{
+		backPage.removeAllViews();
+		backPage.addView(v);
+	}
+
+	public View getBackPage()
+	{
+		return backPage.getChildAt(0);
+	}
+
+	public void setInnerPage(View v)
+	{
+		innerPage.removeAllViews();
+		innerPage.addView(v);
+	}
+
+	public View getInnerPage()
+	{
+		return innerPage.getChildAt(0);
+	}
+
+	public void setDuration(long duration)
+	{
+		flipDuration = duration;
+	}
+
+	public long getDuration()
+	{
+		return flipDuration;
+	}
+
+	private void flip(final View front, final View back)
+	{
+		if (isFlipping) { return; }
+		isFlipping = true;
+		final View visiblePage, invisiblePage;
+		float[] animation1Degree, animation2Degree;
+		if (front.getVisibility() == VISIBLE)
+		{
+			animation1Degree = ROTATE_FRONT_DEGREE1;
+			animation2Degree = ROTATE_FRONT_DEGREE2;
+			visiblePage = front;
+			invisiblePage = back;
+			visiblePage.setPivotX(0);
+			invisiblePage.setPivotX(pageWidth);
+		}
+		else
+		{
+			animation1Degree = ROTATE_BACK_DEGREE1;
+			animation2Degree = ROTATE_BACK_DEGREE2;
+			visiblePage = back;
+			invisiblePage = front;
+			visiblePage.setPivotX(pageWidth);
+			invisiblePage.setPivotX(0);
+		}
+		visiblePage.setPivotY(pivotY);
+		invisiblePage.setPivotY(pivotY);
+		ObjectAnimator animation1 = ObjectAnimator.ofFloat(visiblePage, "rotationY",
+				animation1Degree);
+		animation1.setDuration(flipDuration);
+		animation1.setInterpolator(ACCELERATOR);
+		final ObjectAnimator animation2 = ObjectAnimator.ofFloat(invisiblePage, "rotationY",
+				animation2Degree);
+		animation2.setDuration(flipDuration);
+		animation2.setInterpolator(DECELERATOR);
+		animation1.addListener(new AnimatorListenerAdapter()
+		{
+			@Override
+			public void onAnimationEnd(Animator anim)
+			{
+				visiblePage.setVisibility(View.INVISIBLE);
+				invisiblePage.setVisibility(View.VISIBLE);
+				animation2.start();
+			}
+		});
+		animation2.addListener(new AnimatorListenerAdapter()
+		{
+			@Override
+			public void onAnimationEnd(Animator anim)
+			{
+				isFlipping = false;
+			}
+		});
+		animation1.start();
+	}
+}
--- src/eoc/studio/voicecard/animation/TestAnimationActivity.java
+++ src/eoc/studio/voicecard/animation/TestAnimationActivity.java
@@ -1,119 +1,45 @@
 package eoc.studio.voicecard.animation;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
-import android.view.animation.Interpolator;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import eoc.studio.voicecard.BaseActivity;
 import eoc.studio.voicecard.R;
 
-public class TestAnimationActivity extends BaseActivity implements OnTouchListener
+public class TestAnimationActivity extends BaseActivity
 {
-	/*
-	 * TODO 
-	 * 1. bug on first flip
-	 * 2. refine duplicate code
-	 * 3. extends Layout to wrap these animations
-	 */
-	
-	private Interpolator accelerator = new AccelerateInterpolator();
-	private Interpolator decelerator = new DecelerateInterpolator();
-	private boolean isFlipping = false;
-	private View frontPage;
-	private View backPage;
+	private FlipView card;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		setContentView(R.layout.activity_test_animation);
-		frontPage = findViewById(R.id.front);
-		backPage = findViewById(R.id.back);
-		frontPage.setOnTouchListener(this);
-		backPage.setOnTouchListener(this);
+		
+		FrameLayout frame = (FrameLayout) findViewById(R.id.card);
+		card = new FlipView(this, 380, 250, -45f, 0f, 250/2);
+		frame.addView(card);
+		TextView front = generateTextView("front 1", Color.BLUE, 20, Color.CYAN);
+		TextView back = generateTextView("back 2", Color.RED, 20, Color.MAGENTA);
+		TextView inner = generateTextView("inner 3", Color.WHITE, 20, Color.BLACK);
+		card.setFrontPage(front);
+		card.setBackPage(back);
+		card.setInnerPage(inner);
 		super.onCreate(savedInstanceState);
 	}
 
-	@Override
-	public boolean onTouch(View v, MotionEvent event)
+	private TextView generateTextView(String msg, int color, int textSize, int bgColor)
 	{
-		if (!isFlipping)
-		{
-			flip(frontPage, backPage);
-		}
-		return false;
-	}
-
-	private void flip(final View front, final View back)
-	{
-		isFlipping = true;
-		if (front.getVisibility() == View.VISIBLE)
-		{
-			ObjectAnimator animation1 = ObjectAnimator.ofFloat(front, "rotationY", 0f, -90f);
-			front.setRotationY(0);
-			front.setPivotX(0);
-			animation1.setDuration(400);
-			animation1.setInterpolator(accelerator);
-			final ObjectAnimator animation2 = ObjectAnimator.ofFloat(back, "rotationY", 90f, 0f);
-			back.setPivotX(back.getWidth());
-			animation2.setDuration(400);
-			animation2.setInterpolator(decelerator);
-			animation1.addListener(new AnimatorListenerAdapter()
-			{
-				@Override
-				public void onAnimationEnd(Animator anim)
-				{
-					front.setVisibility(View.INVISIBLE);
-					back.setRotationY(0);
-					back.setVisibility(View.VISIBLE);
-					animation2.start();
-				}
-			});
-			animation2.addListener(new AnimatorListenerAdapter()
-			{
-				@Override
-				public void onAnimationEnd(Animator anim)
-				{
-					isFlipping = false;
-				}
-			});
-			animation1.start();
-		}
-		else
-		{
-			ObjectAnimator animation1 = ObjectAnimator.ofFloat(back, "rotationY", 0f, 90f);
-			back.setPivotX(back.getWidth());
-			animation1.setDuration(400);
-			animation1.setInterpolator(accelerator);
-			final ObjectAnimator animation2 = ObjectAnimator.ofFloat(front, "rotationY", -90f, 0f);
-			front.setPivotX(0);
-			animation2.setDuration(400);
-			animation2.setInterpolator(decelerator);
-			animation1.addListener(new AnimatorListenerAdapter()
-			{
-				@Override
-				public void onAnimationEnd(Animator anim)
-				{
-					back.setVisibility(View.INVISIBLE);
-					front.setRotationY(0);
-					front.setVisibility(View.VISIBLE);
-					animation2.start();
-				}
-			});
-			animation2.addListener(new AnimatorListenerAdapter()
-			{
-				@Override
-				public void onAnimationEnd(Animator anim)
-				{
-					isFlipping = false;
-				}
-			});
-			animation1.start();
-		}
+		TextView tv = new TextView(this);
+		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		tv.setGravity(Gravity.CENTER);
+		tv.setLayoutParams(params);
+		tv.setText(msg);
+		tv.setTextSize(textSize);
+		tv.setTextColor(color);
+		tv.setBackgroundColor(bgColor);
+		return tv;
 	}
 }
