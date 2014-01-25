a2ce07a8d6800c8c43b2d244046da2c7c433c9ef
shin0202 <svnzkimo@outeki-MacBook-Air.local>
2014-01-25 22:13:29 +0800

    add facebook activity, manager, libs
    add facebook activity, manager, libs

--- src/eoc/studio/voicecard/TestMainActivity.java
+++ src/eoc/studio/voicecard/TestMainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import eoc.studio.voicecard.animation.TestAnimationActivity;
+import eoc.studio.voicecard.facebook.TestFacebookActivity;
 
 public class TestMainActivity extends BaseActivity
 {
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
+	    super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test_main);
 		findViewById(R.id.test_animation).setOnClickListener(new OnClickListener()
 		{
@@ -21,6 +23,15 @@ public class TestMainActivity extends BaseActivity
 				startActivity(intent);
 			}
 		});
-		super.onCreate(savedInstanceState);
+		
+        findViewById(R.id.test_facebook).setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(TestMainActivity.this, TestFacebookActivity.class);
+                startActivity(intent);
+            }
+        });
 	}
 }
