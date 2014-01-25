44a1f2e360d06e085f8228ec6fe409e284192d86
shin0202 <svnzkimo@outeki-MacBook-Air.local>
2014-01-25 22:17:55 +0800

    Revert "add facebook activity, manager, libs"
    This reverts commit a2ce07a8d6800c8c43b2d244046da2c7c433c9ef.

--- src/eoc/studio/voicecard/TestMainActivity.java
+++ src/eoc/studio/voicecard/TestMainActivity.java
@@ -5,14 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import eoc.studio.voicecard.animation.TestAnimationActivity;
-import eoc.studio.voicecard.facebook.TestFacebookActivity;
 
 public class TestMainActivity extends BaseActivity
 {
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-	    super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test_main);
 		findViewById(R.id.test_animation).setOnClickListener(new OnClickListener()
 		{
@@ -23,15 +21,6 @@ public class TestMainActivity extends BaseActivity
 				startActivity(intent);
 			}
 		});
-		
-        findViewById(R.id.test_facebook).setOnClickListener(new OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(TestMainActivity.this, TestFacebookActivity.class);
-                startActivity(intent);
-            }
-        });
+		super.onCreate(savedInstanceState);
 	}
 }
