7ecacc8e157c6ed1bba20389d303a17301bee075
Ryan <ryan@DigiNet-MacBook-Pro.local>
2014-03-12 00:45:38 +0800

    [calendar]Add Button function(finish) & Change Button style.

--- src/eoc/studio/voicecard/calendarview/MainCalendarView.java
+++ src/eoc/studio/voicecard/calendarview/MainCalendarView.java
@@ -18,113 +18,111 @@ import android.widget.Button;
 
 public class MainCalendarView extends FragmentActivity implements OnClickListener
 {
-	private static String TAG = "MainCalendarView";
-	private static Context mContext;
-	private static Button btnBack, btnAddFacebookFriendsBirthday, btnBackMainMenu;
+    private static String TAG = "MainCalendarView";
+    private static Context mContext;
+    private static Button btnBack, btnAddFacebookFriendsBirthday, btnBackMainMenu;
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		mContext = this;
-		CalendarIntentHelper.addVoiceCardCalendar(mContext);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_main_calendar_view);
-		findView();
-		buttonFunction();
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        CalendarIntentHelper.addVoiceCardCalendar(mContext);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main_calendar_view);
+        findView();
+        buttonFunction();
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	private void findView()
-	{
-		btnBack = (Button) findViewById(R.id.btnBack);
-		btnAddFacebookFriendsBirthday = (Button) findViewById(R.id.btnAddFacebookFriends);
-		btnBackMainMenu = (Button) findViewById(R.id.btnBackMainMenu);
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void findView()
+    {
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnAddFacebookFriendsBirthday = (Button) findViewById(R.id.btnAddFacebookFriends);
+        btnBackMainMenu = (Button) findViewById(R.id.btnBackMainMenu);
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	public void buttonFunction()
-	{
-		btnBack.setOnClickListener(this);
-		btnAddFacebookFriendsBirthday.setOnClickListener(this);
-		btnBackMainMenu.setOnClickListener(this);
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void buttonFunction()
+    {
+        btnBack.setOnClickListener(this);
+        btnAddFacebookFriendsBirthday.setOnClickListener(this);
+        btnBackMainMenu.setOnClickListener(this);
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	public void onClick(View view)
-	{
-		switch (view.getId())
-		{
-		case R.id.btnBack:
-		{
-			System.out.println("*****************btnBack");
-		}
-			break;
-		case R.id.btnAddFacebookFriends:
-		{
-			addFacebookFriends();
-			System.out.println("*****************btnAddFacebookFriends");
-		}
-			break;
-		case R.id.btnBackMainMenu:
-		{
-			System.out.println("*****************btnBackMainMenu");
-		}
-			break;
-		}
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.btnBack:
+            {
+                finish();
+            }
+                break;
+            case R.id.btnAddFacebookFriends:
+            {
+                addFacebookFriends();
+            }
+                break;
+            case R.id.btnBackMainMenu:
+            {
+                finish();
+            }
+                break;
+        }
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-		super.onActivityResult(requestCode, resultCode, data);
-		switch (requestCode)
-		{
-		case FriendInfo.GET_FRIEND_REQUEST_CODE:
-			if (data != null)
-			{
-				Bundle bundle = data.getExtras();
-				if (bundle != null)
-				{
-					ArrayList<FriendInfo> friendList = bundle
-							.getParcelableArrayList(FriendInfo.GET_FRIEND);
-					if (friendList != null)
-					{
-						int j = 1;
-						Log.d(TAG, "********Total Count" + friendList.size());
-						for (int i = 0; i < friendList.size(); i++)
-						{
-							if (friendList.get(i).getFriendBirthday() != null
-									&& !friendList.get(i).getFriendBirthday().equals("null"))
-							{
-								Log.d(TAG, "*****************add count " + j++);
-								Log.d(TAG, "ID is " + friendList.get(i).getFriendId());
-								Log.d(TAG, "Name is " + friendList.get(i).getFriendName());
-								Log.d(TAG, "Birthday is " + friendList.get(i).getFriendBirthday());
-								Log.d(TAG, "ImgLink is " + friendList.get(i).getFriendImgLink());
-								//
-								String getNewFormatOfBirthday = DataProcess.formatDate(DataProcess
-										.checkFacebookBirthdayFormats(friendList.get(i)
-												.getFriendBirthday()), "yyyyMMdd");
-								DataProcess.addEvent(mContext, friendList.get(i).getFriendName()
-										+ "'s Birthday", getNewFormatOfBirthday);
-							}
-						}
-					}
-				}
-			}
-			break;
-		}
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            case FriendInfo.GET_FRIEND_REQUEST_CODE:
+                if (data != null)
+                {
+                    Bundle bundle = data.getExtras();
+                    if (bundle != null)
+                    {
+                        ArrayList<FriendInfo> friendList = bundle.getParcelableArrayList(FriendInfo.GET_FRIEND);
+                        if (friendList != null)
+                        {
+                            int j = 1;
+                            Log.d(TAG, "********Total Count" + friendList.size());
+                            for (int i = 0; i < friendList.size(); i++)
+                            {
+                                if (friendList.get(i).getFriendBirthday() != null
+                                        && !friendList.get(i).getFriendBirthday().equals("null"))
+                                {
+                                    Log.d(TAG, "*****************add count " + j++);
+                                    Log.d(TAG, "ID is " + friendList.get(i).getFriendId());
+                                    Log.d(TAG, "Name is " + friendList.get(i).getFriendName());
+                                    Log.d(TAG, "Birthday is " + friendList.get(i).getFriendBirthday());
+                                    Log.d(TAG, "ImgLink is " + friendList.get(i).getFriendImgLink());
+                                    //
+                                    String getNewFormatOfBirthday = DataProcess.formatDate(DataProcess
+                                            .checkFacebookBirthdayFormats(friendList.get(i).getFriendBirthday()),
+                                            "yyyyMMdd");
+                                    DataProcess.addEvent(mContext, friendList.get(i).getFriendName() + "'s Birthday",
+                                            getNewFormatOfBirthday);
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	private static void addFacebookFriends()
-	{
-		Intent intent = new Intent(MainCalendarView.mContext, SelectFriendActivity.class);
-		((Activity) mContext).startActivityForResult(intent, FriendInfo.GET_FRIEND_REQUEST_CODE);
-	}
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private static void addFacebookFriends()
+    {
+        Intent intent = new Intent(MainCalendarView.mContext, SelectFriendActivity.class);
+        ((Activity) mContext).startActivityForResult(intent, FriendInfo.GET_FRIEND_REQUEST_CODE);
+    }
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
--- src/eoc/studio/voicecard/calendarview/SetCalendarMainView.java
+++ src/eoc/studio/voicecard/calendarview/SetCalendarMainView.java
@@ -12,63 +12,59 @@ import android.widget.TextView;
 
 public class SetCalendarMainView extends FragmentActivity implements OnClickListener
 {
-	private static Context mContext;
-	private static TextView titleTextView;
-	private static Button addButton, confirmButton;
+    private static Context mContext;
+    private static TextView titleTextView;
+    private static Button addButton, confirmButton;
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		mContext = this;
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		Bundle bundle = this.getIntent().getExtras();
-		DataProcess.setSelectedEventDate((String) bundle.getString(DataProcess.EVENT_DATE)
-				.toString());
-		setContentView(R.layout.activity_set_calendar_main_view);
-		findView();
-		buttonFunction();
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Bundle bundle = this.getIntent().getExtras();
+        DataProcess.setSelectedEventDate((String) bundle.getString(DataProcess.EVENT_DATE).toString());
+        setContentView(R.layout.activity_set_calendar_main_view);
+        findView();
+        buttonFunction();
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	private void findView()
-	{
-		titleTextView = (TextView) findViewById(R.id.titleTextView);
-		addButton = (Button) findViewById(R.id.buttonConfirmView);
-		confirmButton = (Button) findViewById(R.id.buttonAddView);
-		titleTextView.setText(DataProcess.formatDate(
-				DataProcess.getDataMilliSeconds(DataProcess.getSelectedEventDate()),
-				"yyyy-MM-dd(E)"));
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void findView()
+    {
+        titleTextView = (TextView) findViewById(R.id.titleTextView);
+        addButton = (Button) findViewById(R.id.buttonBackView);
+        confirmButton = (Button) findViewById(R.id.buttonAddView);
+        titleTextView.setText(DataProcess.formatDate(
+                DataProcess.getDataMilliSeconds(DataProcess.getSelectedEventDate()), "yyyy-MM-dd(E)"));
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	private void buttonFunction()
-	{
-		addButton.setOnClickListener(this);
-		confirmButton.setOnClickListener(this);
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void buttonFunction()
+    {
+        addButton.setOnClickListener(this);
+        confirmButton.setOnClickListener(this);
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	public void onClick(View view)
-	{
-		switch (view.getId())
-		{
-		case R.id.buttonConfirmView:
-		{
-			finish();
-		}
-			break;
-		case R.id.buttonAddView:
-		{
-			ShowDialog.showSetValueDialog(mContext,
-					DataProcess.formatDate(
-							DataProcess.getDataMilliSeconds(DataProcess.getSelectedEventDate()),
-							"yyyyMMdd"));
-		}
-			break;
-		}
-	}
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.buttonBackView:
+            {
+                finish();
+            }
+                break;
+            case R.id.buttonAddView:
+            {
+                ShowDialog.showSetValueDialog(mContext, DataProcess.formatDate(
+                        DataProcess.getDataMilliSeconds(DataProcess.getSelectedEventDate()), "yyyyMMdd"));
+            }
+                break;
+        }
+    }
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
