4e87eefd0ccff59d494e59c845082b12f943e6c6
shin0202 <svnzkimo@gmail.com>
2014-03-12 16:29:22 +0800

    Voice Card : To modify network receiver(remove from BaseActivity and Enable in Manifest)

--- src/eoc/studio/voicecard/utils/NetworkReceiver.java
+++ src/eoc/studio/voicecard/utils/NetworkReceiver.java
@@ -3,37 +3,20 @@ package eoc.studio.voicecard.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import android.net.ConnectivityManager; 
-import android.net.NetworkInfo.State;
 import android.util.Log;
-import android.widget.Toast; 
 
 public class NetworkReceiver extends BroadcastReceiver {
+    private static final String TAG = "NetworkReceiver";
 
-    State wifiState = null;
-    State mobileState = null;
-    public static final String ACTION = "android.net.conn.CONNECTIVITY_CHANGE";
-
+    private static boolean isOnline = false;
+    
     @Override
     public void onReceive(Context context, Intent intent) {
-//        Log.d("Test", "intent is " + intent);
-//        Log.d("Test", "action is " + intent.getAction());
-//        if (ACTION.equals(intent.getAction())) {
-//            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-//            wifiState = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
-//            mobileState = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState();
-//            if (wifiState != null && mobileState != null && State.CONNECTED != wifiState
-//                    && State.CONNECTED == mobileState) {
-//                Toast.makeText(context, "手機網路連接成功！", Toast.LENGTH_SHORT).show();
-//            } else if (wifiState != null && mobileState != null && State.CONNECTED == wifiState
-//                    && State.CONNECTED != mobileState) {
-//                Toast.makeText(context, "無線網路連接成功！", Toast.LENGTH_SHORT).show();
-//            } else if (wifiState != null && mobileState != null && State.CONNECTED != wifiState
-//                    && State.CONNECTED != mobileState) {
-//                Toast.makeText(context, "手機沒有任何網路...", Toast.LENGTH_SHORT).show();
-//            }
-//        }
+        isOnline = NetworkUtility.isOnline(context);
+        Log.d(TAG, "The network is online ? " + isOnline);
     }
-
+    
+    public static boolean isOnline() {
+        return isOnline;
+    }
 }
