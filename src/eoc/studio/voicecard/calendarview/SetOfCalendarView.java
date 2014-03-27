370f68af23d582bc019d43667f1784f399e20a2b
Ryan <ryan@DigiNet-MacBook-Pro.local>
2014-03-28 03:44:15 +0800

    [Newspaper][Calendar]Add Support HDPI layout.

--- src/eoc/studio/voicecard/calendarview/SetOfCalendarView.java
+++ src/eoc/studio/voicecard/calendarview/SetOfCalendarView.java
@@ -24,137 +24,163 @@ import android.widget.RelativeLayout;
 
 public class SetOfCalendarView extends Fragment implements OnTouchListener
 {
-	private static Context mContext;
-	private static SetOfCalendarAdapter calendarAdapter;
-	private static HorizontalScrollView horizontalScrollView = null;
-	private static float moveStart = 0, moveEnd = 0;
-	private static boolean isSmoothScroll = false;
-	private int getPages = 0;
-	private static ArrayList<Map<String, String>> mData;
-	private static GridView calendarGrid;
-	private static RelativeLayout hideView;
-	private static final int DEFAULT_SCROOL_SITE = 0;
-	private static final int DEFAULT_SCROOL_BUFF = 100;
-	private static final int DEFAULT_NOITE_WIDTH = 435;
-	private static final int ONE_PAGE = 1, THREE_PAGE = 3;
+    private static Context mContext;
+    private static SetOfCalendarAdapter calendarAdapter;
+    private static HorizontalScrollView horizontalScrollView = null;
+    private static float moveStart = 0, moveEnd = 0;
+    private static boolean isSmoothScroll = false;
+    private int getPages = 0;
+    private static ArrayList<Map<String, String>> mData;
+    private static GridView calendarGrid;
+    private static RelativeLayout hideView;
+    private static final int DEFAULT_SCROOL_SITE = 0;
+    private static int DEFAULT_SCROOL_BUFF, DEFAULT_NOITE_WIDTH;
+    private static final int DEFAULT_SCROOL_BUFF_XHDPI = 100;
+    private static final int DEFAULT_NOITE_WIDTH_XHDPI = 435;
+    private static final int DEFAULT_SCROOL_BUFF_HDPI = 75;
+    private static final int DEFAULT_NOITE_WIDTH_HDPI = 326;
+    private static final int ONE_PAGE = 1, THREE_PAGE = 3;
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	public SetOfCalendarView()
-	{
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public SetOfCalendarView()
+    {
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-	{
-		mContext = getActivity();
-		final RelativeLayout calendarLayout = (RelativeLayout) inflater.inflate(
-				R.layout.set_of_calendar, null);
-		hideView = (RelativeLayout) calendarLayout.findViewById(R.id.hideView);
-		horizontalScrollView = (HorizontalScrollView) calendarLayout
-				.findViewById(R.id.calendar_horizontal_scroll);
-		calendarGrid = (GridView) calendarLayout.findViewById(R.id.calendar_grid);
-		horizontalScrollView.setOnTouchListener(this);
-		getNewView();
-		return calendarLayout;
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	protected static void getNewView()
-	{
-		mData = getEventData();
-		if (mData == null) { return; }
-		calendarAdapter = new SetOfCalendarAdapter(mContext, mData);
-		if (mData.size() == 1)
-		{
-			hideView.setVisibility(View.INVISIBLE);
-		}
-		else
-		{
-			hideView.setVisibility(View.GONE);
-		}
-		calendarGrid.setNumColumns(mData.size());
-		calendarGrid.setLayoutParams(new FrameLayout.LayoutParams(535 * mData.size(), 510));
-		calendarGrid.setSelector(new ColorDrawable(Color.TRANSPARENT));
-		calendarGrid.setOnItemClickListener(new DayItemClickListener());
-		calendarGrid.setAdapter(calendarAdapter);
-	}
+        mContext = getActivity();
+        final RelativeLayout calendarLayout = (RelativeLayout) inflater.inflate(R.layout.set_of_calendar, null);
+        hideView = (RelativeLayout) calendarLayout.findViewById(R.id.hideView);
+        horizontalScrollView = (HorizontalScrollView) calendarLayout.findViewById(R.id.calendar_horizontal_scroll);
+        calendarGrid = (GridView) calendarLayout.findViewById(R.id.calendar_grid);
+        horizontalScrollView.setOnTouchListener(this);
+        getNewView();
+        return calendarLayout;
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	public boolean onTouch(View v, MotionEvent event)
-	{
-		if (event.getAction() == MotionEvent.ACTION_MOVE && !isSmoothScroll)
-		{
-			isSmoothScroll = true;
-			moveStart = event.getX();
-		}
-		else if (event.getAction() == MotionEvent.ACTION_UP)
-		{
-			isSmoothScroll = false;
-			moveEnd = event.getX();
-			if (moveStart > moveEnd)
-			{
-				if (getPages >= (mData.size() - 1)) { return true; }
-				smoothScrollView((++getPages));
-			}
-			else
-			{
-				if (getPages < 1) { return false; }
-				smoothScrollView((--getPages));
-			}
-		}
-		return true;
-	}
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected static void getNewView()
+    {
+        float scale = mContext.getResources().getDisplayMetrics().density;
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	private static void smoothScrollView(final int getPage)
-	{
-		new Handler().postDelayed((new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				if (getPage == DEFAULT_SCROOL_SITE)
-				{
-					horizontalScrollView.smoothScrollTo(DEFAULT_SCROOL_SITE, DEFAULT_SCROOL_SITE);
-				}
-				else if (getPage == ONE_PAGE)
-				{
-					horizontalScrollView.smoothScrollTo(DEFAULT_NOITE_WIDTH, DEFAULT_SCROOL_SITE);
-				}
-				else if (getPage < THREE_PAGE)
-				{
-					horizontalScrollView.smoothScrollTo((DEFAULT_NOITE_WIDTH * (getPage))
-							+ DEFAULT_SCROOL_BUFF, DEFAULT_SCROOL_SITE);
-				}
-				else
-				{
-					horizontalScrollView.smoothScrollTo(
-							((DEFAULT_NOITE_WIDTH + DEFAULT_SCROOL_BUFF) * getPage)
-									- DEFAULT_SCROOL_BUFF, DEFAULT_SCROOL_SITE);
-				}
-			}
-		}), 5);
-	}
+        if (scale == 1.5)
+        {
+            DEFAULT_SCROOL_BUFF = DEFAULT_SCROOL_BUFF_HDPI;
+            DEFAULT_NOITE_WIDTH = DEFAULT_NOITE_WIDTH_HDPI;
+        }
+        else
+        {
+            DEFAULT_SCROOL_BUFF = DEFAULT_SCROOL_BUFF_XHDPI;
+            DEFAULT_NOITE_WIDTH = DEFAULT_NOITE_WIDTH_XHDPI;
+        }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	private static ArrayList<Map<String, String>> getEventData()
-	{
-		String event = DataProcess.getSelectedEventDate();
-		if (!event.equals("")) { return CalendarIntentHelper.readCalendarEvent(mContext,
-				DataProcess.getSelectedEventDate()); }
-		return null;
-	}
+        mData = getEventData();
+        if (mData == null)
+        {
+            return;
+        }
+        calendarAdapter = new SetOfCalendarAdapter(mContext, mData);
+        if (mData.size() == 1)
+        {
+            hideView.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            hideView.setVisibility(View.GONE);
+        }
+        calendarGrid.setNumColumns(mData.size());
+        calendarGrid.setLayoutParams(new FrameLayout.LayoutParams((DEFAULT_NOITE_WIDTH + DEFAULT_SCROOL_BUFF)
+                * mData.size(), 510));
+        calendarGrid.setSelector(new ColorDrawable(Color.TRANSPARENT));
+        calendarGrid.setOnItemClickListener(new DayItemClickListener());
+        calendarGrid.setAdapter(calendarAdapter);
+    }
 
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	private static final class DayItemClickListener implements OnItemClickListener
-	{
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-		{
-			calendarAdapter.setSelected(position);
-		}
-	}
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public boolean onTouch(View v, MotionEvent event)
+    {
+        if (event.getAction() == MotionEvent.ACTION_MOVE && !isSmoothScroll)
+        {
+            isSmoothScroll = true;
+            moveStart = event.getX();
+        }
+        else if (event.getAction() == MotionEvent.ACTION_UP)
+        {
+            isSmoothScroll = false;
+            moveEnd = event.getX();
+            if (moveStart > moveEnd)
+            {
+                if (getPages >= (mData.size() - 1))
+                {
+                    return true;
+                }
+                smoothScrollView((++getPages));
+            }
+            else
+            {
+                if (getPages < 1)
+                {
+                    return false;
+                }
+                smoothScrollView((--getPages));
+            }
+        }
+        return true;
+    }
+
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private static void smoothScrollView(final int getPage)
+    {
+        new Handler().postDelayed((new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                if (getPage == DEFAULT_SCROOL_SITE)
+                {
+                    horizontalScrollView.smoothScrollTo(DEFAULT_SCROOL_SITE, DEFAULT_SCROOL_SITE);
+                }
+                else if (getPage == ONE_PAGE)
+                {
+                    horizontalScrollView.smoothScrollTo(DEFAULT_NOITE_WIDTH, DEFAULT_SCROOL_SITE);
+                }
+                else if (getPage < THREE_PAGE)
+                {
+                    horizontalScrollView.smoothScrollTo((DEFAULT_NOITE_WIDTH * (getPage)) + DEFAULT_SCROOL_BUFF,
+                            DEFAULT_SCROOL_SITE);
+                }
+                else
+                {
+                    horizontalScrollView.smoothScrollTo(((DEFAULT_NOITE_WIDTH + DEFAULT_SCROOL_BUFF) * getPage)
+                            - DEFAULT_SCROOL_BUFF, DEFAULT_SCROOL_SITE);
+                }
+            }
+        }), 5);
+    }
+
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private static ArrayList<Map<String, String>> getEventData()
+    {
+        String event = DataProcess.getSelectedEventDate();
+        if (!event.equals(""))
+        {
+            return CalendarIntentHelper.readCalendarEvent(mContext, DataProcess.getSelectedEventDate());
+        }
+        return null;
+    }
+
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private static final class DayItemClickListener implements OnItemClickListener
+    {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            calendarAdapter.setSelected(position);
+        }
+    }
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
